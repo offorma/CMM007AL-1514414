bcf4e885a93e7c8b7c854a0df2bdfdd69700d5e0
Chukwunonso <offorma@gmail.com>
2016-05-03 16:06:40 +0100

    test

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: offormachukwunonso
+ * Date: 03/05/2016
+ * Time: 16:02
+ */
+echo "may the force be with you";
+
+$con = mysqli_connect('ap-cdbr-azure-east-c.cloudapp.net','bba3ec9170e818','a2edc6cd','CMM007Al-1514414');
+if ($con){
+    echo 'true';
+}
+?>
