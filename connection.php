72701b17473315231504b5598dc7c465763a85c0
offorma <offorma@gmail.com>
2016-05-04 11:03:50 +0100

    init

--- connection.php
+++ connection.php
@@ -5,12 +5,7 @@
  * Date: 04/05/2016
  * Time: 10:37
  */
-define('HOST', 'ap-cdbr-azure-east-c.cloudapp.net');
-define('USERNAME','bba3ec9170e818');
-define('PASSWORD','a2edc6cd');
-define('DB','CMM007Al-1514414');
-
-$con = mysqli_connect('HOST','USERNAME','PASSWORD','DB');
+$con = mysqli_connect('ap-cdbr-azure-east-c.cloudapp.net','bba3ec9170e818','a2edc6cd','CMM007Al-1514414');
 if ($con){
     echo 'true';
 }else{
